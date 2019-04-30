05c8e77b7d4497d410da617cd989ae5e9aa64738
16105067 <16105067@l1818319.portoalegre.pucrsnet.br>
2019-04-30 10:11:22 -0300

    Adicionados sintaxe e semântica do swap

--- src/Swap.java
+++ src/Swap.java
@@ -1,3 +1,27 @@
+/*
+ * Sintaxe:
+ * 
+ * swap(x_1, x_2), onde x_n pertence a Var
+ * 
+ * 	c ::= skip 
+ * 		| x := a 
+ * 		| c_1 ; c_2
+ * 		| while b do c
+ * 		| if b then c_1 else c_2
+ * 		| swap(x_1, x_2)
+ * 
+ * Semântica:
+ * 	Big-Step:
+ * 		<x1, s> => n1            <x2, s> => n2
+ *     ----------------------------------------swap
+ *        <swap(x1,x2), s> => s[x1/n2, x2/n1]
+ * 
+ *	Small-Step:
+ *		
+ *		-------------------------------------------swap
+ *	     <swap(x1,x2), s> -> s[x1/s(x2), x2/s(x1)]
+ * */
+
 public class Swap extends Prog {
 	private Var e1;
 	private Var e2;
